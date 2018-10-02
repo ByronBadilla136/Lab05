fc9c483994c19441eedbb6231f7d2c693b38dcd2
Byron Badilla <byronbadilla@outlook.com>
2018-10-01 19:17:05 -0600

    Lab05

---
+++ Lab05/Lab05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" HERENCIA ");
+            PrintPerson(new Student());
+
+            Console.WriteLine(" STUDENTC ");
+            PrintStudent(new StudentC());
+
+            Console.ReadKey();
+
+
+    }
+
+    public static void PrintPerson(Student student){
+                
+         Console.Write(student.FormatIdAndName(" John ", 9));
+              }
+        public static void PrintStudent(StudentC student)
+        {
+
+            Console.Write(student.FormatIdAndName(" Pablo ", 3));
+        }
+    }
+}
